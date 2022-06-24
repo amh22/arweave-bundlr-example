0d38d509d2b96acecc2aa5106927d9219b73082f
Andrew Henry <andrewmhenry@me.com>
2022-06-24 20:23:05 +1000

    🚀 feat(Initial Commit): initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import { providers, utils } from 'ethers'
+import { WebBundlr } from '@bundlr-network/client'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  async function initialise() {
+    await window.ethereum.enable()
+    const provider = new providers.Web3Provider(window.ethereum)
+    await provider._ready()
+
+    // interact with bundlr in the client
+    // allowing for signing by the user
+    const bundlr = new WebBundlr('https://node1.bundlr.network', 'matic', provider)
+    await bundlr.ready()
+  }
+  return (
+    <div>
+      <button onClick={initialise}>Initialise</button>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
