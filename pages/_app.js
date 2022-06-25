1be526f57003c0fa7c1ab30de3087aa4fc56db82
Andrew Henry <andrewmhenry@me.com>
2022-06-25 19:26:08 +1000

    🚀 feat(Funding and File upload): implemented Fund of Bundlr node and file upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import { providers, utils } from 'ethers'
 import { WebBundlr } from '@bundlr-network/client'
+import { useState, useRef } from 'react'
+import { MainContext } from '../context'
 
 function MyApp({ Component, pageProps }) {
+  const [bundlrInstance, setBundlrInstance] = useState()
+  const [balance, setBalance] = useState()
+  const bundlrRef = useRef()
+
+  // initialise bundlr
   async function initialise() {
     await window.ethereum.enable()
     const provider = new providers.Web3Provider(window.ethereum)
@@ -12,13 +19,37 @@ function MyApp({ Component, pageProps }) {
     // allowing for signing by the user
     const bundlr = new WebBundlr('https://node1.bundlr.network', 'matic', provider)
     await bundlr.ready()
+    setBundlrInstance(bundlr)
+    bundlrRef.current = bundlr
+    fetchBalance()
   }
+
+  async function fetchBalance() {
+    const bal = await bundlrRef.current.getLoadedBalance()
+    console.log('🚀 ~ fetchBalance ~ bal', utils.formatEther(bal.toString()))
+    setBalance(utils.formatEther(bal.toString()))
+  }
+
   return (
-    <div>
-      <button onClick={initialise}>Initialise</button>
-      <Component {...pageProps} />
+    <div style={containerStyle}>
+      <MainContext.Provider
+        value={{
+          initialise,
+          fetchBalance,
+          balance,
+          bundlrInstance,
+        }}
+      >
+        <Component {...pageProps} />
+      </MainContext.Provider>
     </div>
   )
+}
+
+const containerStyle = {
+  width: '600px',
+  margin: '0 auto',
+  padding: '40px',
 }
 
 export default MyApp
